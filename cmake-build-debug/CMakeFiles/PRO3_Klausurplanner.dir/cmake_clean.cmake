3d2510fd4687bc1a0dde511b59fb10dd5b486755
81mide1bif <81mide1bif@hft-stuttgart.de>
2020-06-13 15:10:04 +0200

    hello world

---
+++ cmake-build-debug/CMakeFiles/PRO3_Klausurplanner.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PRO3_Klausurplanner.dir/main.cpp.o"
+  "PRO3_Klausurplanner"
+  "PRO3_Klausurplanner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PRO3_Klausurplanner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
